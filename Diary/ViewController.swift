7022c39dd9265932ebbf9b78aca296cd3387f1ec
Shaun McCarthy <shaunmccarthy1@gmail.com>
2019-06-10 13:00:59 +0930

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Shaun McCarthy on 10/6/19.
+//  Copyright © 2019 Shaun McCarthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
